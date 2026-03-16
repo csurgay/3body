514b036b25f88f2000b4c06eaf67c60c3e2ffbf5
Peter Csurgay <csurgay@gmail.com>
2026-03-16 16:01:56 +0100

    Add future track too

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const DEBUG = 0;
 let running = true;
 let NO_PLANETS = 3;
 const weights = [1, 1, 1, 1, 1, 1, 1, 1];
@@ -5,7 +6,9 @@ let canvas=null;
 let ctx=null;
 let bodies = [];
 let tracks = [];
+let future = [];
 let L_TRACK = 200;
+let L_FUTURE = 200;
 const COLOR = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff', '#000000', '#ffffff'];
 let cx = cy = 0;
 let hotSpots = []; // x, y, type ("body" or "vector")
@@ -25,17 +28,19 @@ class Body {
 
 function pause() {
     running = !running;
+    if (DEBUG > 0) console.log('pause');
 }
 
 function main() {
-    console.log('init');
+    if (DEBUG > 0) console.log('main');
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     for (let i = 1; i <= 8; i++) {
         document.getElementById("B" + i).style.color = COLOR[i-1];
     }
-    window.addEventListener('keypress', function(event) {
-        if (event.key === ' ') {
+    window.addEventListener('keyup', function(event) {
+        if (DEBUG > 0) console.log("key '" + event.key + "'");
+        if (event.key == ' ') {
             pause();
         }
     });
@@ -80,8 +85,10 @@ function main() {
 }
 
 function init() {
+    if (DEBUG > 0) console.log('init');
     bodies = [];
     tracks = [];
+    future = [];
     // Get the number of planets from the input field
     NO_PLANETS = parseInt(document.getElementById('bodies').value);
     // Create random planets into bodies array
@@ -128,50 +135,51 @@ function adjustBodies(j) {
 
 function animate() {
     if (running) {
+        L_TRACK = parseInt(document.getElementById('track').value);
         for (let i = 0; i < document.getElementById("speed").value; i++) {
-            calc();
+            bodies = JSON.parse(calc(JSON.stringify(bodies))); // Deep copy of bodies array
+            // Add the current position to the track
+            for (let i = 0; i < bodies.length; i++) {
+                tracks[i].push({x: bodies[i].x, y: bodies[i].y});
+                // Limit the length of the track
+                while (tracks[i].length > L_TRACK) {
+                    tracks[i].shift();
+                }
+            }
         }
     }
     draw();
     requestAnimationFrame(animate);
 }
 
-function calc() {
-    L_TRACK = parseInt(document.getElementById('track').value);
+function calc(pbodies) {
+    let tbodies = JSON.parse(pbodies); // Deep copy of bodies array
     // Calculate the new speed vectors from the gravitational forces between bodies
-    for (let i = 0; i < bodies.length; i++) {
-        let p = bodies[i];
-        for (let j=0; j<bodies.length; j++) {
+    for (let i = 0; i < tbodies.length; i++) {
+        for (let j=0; j<tbodies.length; j++) {
             if (i !== j) {
-                let p2 = bodies[j];
-                let dx = p2.x - p.x;
-                let dy = p2.y - p.y;
+                let p2 = tbodies[j];
+                let dx = p2.x - tbodies[i].x;
+                let dy = p2.y - tbodies[i].y;
                 let r2 = dx*dx + dy*dy;
                 if (r2 < 10) r2 = 10; // Avoid too large forces at close distances
-                let force = p.weight * p2.weight / r2;
-                p.vx += dx * force;
-                p.vy += dy * force;
+                let force = tbodies[i].weight * p2.weight / r2;
+                tbodies[i].vx += dx * force;
+                tbodies[i].vy += dy * force;
             }
         }
     }
     // Update the positions of the bodies based on their speed vectors
-    for (let i = 0; i < bodies.length; i++) {
-        let p = bodies[i];
-        p.x += p.vx / p.weight;
-        p.y += p.vy / p.weight;
+    for (let i = 0; i < tbodies.length; i++) {
+        tbodies[i].x += tbodies[i].vx / tbodies[i].weight;
+        tbodies[i].y += tbodies[i].vy / tbodies[i].weight;
     }
     // Update the positions of the bodies based on their speed vectors
-    for (let i = 0; i < bodies.length; i++) {
-        let p = bodies[i];
-        p.x += p.vx / p.weight;
-        p.y += p.vy / p.weight;
-        // Add the current position to the track
-        tracks[i].push({x: p.x, y: p.y});
-        // Limit the length of the track
-        while (tracks[i].length > L_TRACK) {
-            tracks[i].shift();
-        }
+    for (let i = 0; i < tbodies.length; i++) {
+        tbodies[i].x += tbodies[i].vx / tbodies[i].weight;
+        tbodies[i].y += tbodies[i].vy / tbodies[i].weight;
     }
+    return JSON.stringify(tbodies);
 }
 
 function draw() {
@@ -210,14 +218,16 @@ function draw() {
         ctx.fill();
         hotSpots.push({x: p.x, y: p.y, type: "body", i: i});
         // Draw track reverse
-        ctx.beginPath();
-        ctx.moveTo(p.x, p.y);
-        for (let j = tracks[i].length - 1; j >= 0; j--) {
-            ctx.lineTo(tracks[i][j].x, tracks[i][j].y);
+        if (running) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            for (let j = tracks[i].length - 1; j >= 0; j--) {
+                ctx.lineTo(tracks[i][j].x, tracks[i][j].y);
+            }
+            ctx.strokeStyle = p.color;
+            ctx.lineWidth = 1;
+            ctx.stroke();
         }
-        ctx.strokeStyle = p.color;
-        ctx.lineWidth = 1;
-        ctx.stroke();
     }
     // Speed vector arrows when paused
     if (!running) {
@@ -226,6 +236,27 @@ function draw() {
             drawArrow(p.x, p.y, p.x + p.vx * 100, p.y + p.vy * 100);
             hotSpots.push({x: p.x + p.vx * 100, y: p.y + p.vy * 100, type: "vector", i: i});
         }
+        // Calculate future positions when paused
+        future = [];
+        let stringBodies = JSON.stringify(bodies);
+        for (let step=0; step<document.getElementById("future").value; step++) {
+            stringBodies = calc(stringBodies);
+            future.push(stringBodies);
+        }
+        // Draw future tracks
+        ctx.setLineDash([3, 3]);
+        ctx.lineWidth = 1;
+        for (let i = 0; i < bodies.length; i++) {
+            ctx.beginPath();
+            ctx.moveTo(bodies[i].x, bodies[i].y);
+            for (let step=0; step<future.length; step++) {
+                let fbodies = JSON.parse(future[step]);
+                ctx.lineTo(fbodies[i].x, fbodies[i].y);
+            }
+            ctx.strokeStyle = bodies[i].color;
+            ctx.stroke();
+        }
+        ctx.setLineDash([]);
     }
 }
 
